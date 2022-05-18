5b43fa3d1e9098e28a7d5ad42e36179bb445974d
monsaw <lawalmonsaw@gmail.com>
2022-05-18 11:35:08 +0100

    Push Comment 1

--- src/src/main/java/org/example/Currency.java
+++ src/src/main/java/org/example/Currency.java
@@ -7,6 +7,15 @@ public class Currency extends Currencies
     //Currencies currencies;
 
 
+    public Currency() {
 
+        getWithdrawal().put("500", 5);
+        getWithdrawal().put("200", 3);
+        getWithdrawal().put("100", 6);
+        getWithdrawal().put("50", 2);
+        getWithdrawal().put("20", 5);
+        getWithdrawal().put("10", 1);
+        getWithdrawal().put("5", 4);
 
+    }
 }
